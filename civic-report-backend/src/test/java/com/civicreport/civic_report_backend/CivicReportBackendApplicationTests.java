b3c3def9f898611bf571bcaebba5a09739c9499d
Deepa Meti <deepameti2005@gmail.com>
2026-08-18 20:53:31 +0530

    add spring boot backend project

---
+++ civic-report-backend/src/test/java/com/civicreport/civic_report_backend/CivicReportBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.civicreport.civic_report_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CivicReportBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
